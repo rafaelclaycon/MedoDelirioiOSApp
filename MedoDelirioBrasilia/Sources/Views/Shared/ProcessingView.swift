122a95b3d5019315532d221542dd2a9fda742776
Rafael Schmitt <rafaelschmitt@fastmail.com>
2026-06-12 02:00:47 -0300

    Clean up ProcessingView

--- MedoDelirioBrasilia/Sources/Views/Shared/ProcessingView.swift
+++ MedoDelirioBrasilia/Sources/Views/Shared/ProcessingView.swift
@@ -1,12 +1,19 @@
+//
+//  ProcessingView.swift
+//  MedoDelirioBrasilia
+//
+//  Created by Rafael Schmitt on 21/08/22.
+//
+
 import SwiftUI
 
 struct ProcessingView: View {
 
-    @State var message: String
-    @State var progressViewYOffset: CGFloat = -20
-    @State var progressViewWidth: CGFloat = 200
-    @State var messageYOffset: CGFloat = 33
-    
+    let message: String
+    var progressViewYOffset: CGFloat = -20
+    var progressViewWidth: CGFloat = 200
+    var messageYOffset: CGFloat = 33
+
     var body: some View {
         ZStack {
             ProgressView()
@@ -24,14 +31,13 @@ struct ProcessingView: View {
 
 }
 
-struct ProcessandoView_Previews: PreviewProvider {
-
-    static var previews: some View {
-        ForEach(ColorScheme.allCases, id: \.self) {
-            //ProcessingView(message: Shared.ShareAsVideo.generatingVideoShortMessage).preferredColorScheme($0)
-            ProcessingView(message: Shared.ShareAsVideo.generatingVideoLongMessage, progressViewYOffset: -27, progressViewWidth: 270, messageYOffset: 30).preferredColorScheme($0)
-        }
-        .previewLayout(.fixed(width: 375, height: 400))
+#Preview {
+    VStack(spacing: .spacing(.xLarge)) {
+        ProcessingView(
+            message: Shared.ShareAsVideo.generatingVideoLongMessage,
+            progressViewYOffset: -27,
+            progressViewWidth: 270,
+            messageYOffset: 30
+        )
     }
-
 }
